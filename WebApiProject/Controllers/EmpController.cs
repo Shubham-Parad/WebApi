4ae5d6d3d55c84fe6e121797a35f1d07969e3780
Shubham-Parad <shubhamparad19@gmail.com>
2024-08-30 17:24:19 +0530

    crud operation using webapi

---
+++ WebApiProject/Controllers/EmpController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiProject.Data;
+using WebApiProject.Models;
+
+namespace WebApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        private readonly ApplicationDbContext db;
+        public EmpController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [Route("AddEmp")]
+        [HttpPost]
+        public IActionResult AddNewEmp(Emp e)
+        {
+            db.employees.Add(e);
+            db.SaveChanges();
+            return Ok("Emp Added Successfully!!!");
+        }
+
+        [Route("GetEmps")]
+        [HttpGet]
+        public IActionResult GetAllEmpData()
+        {
+            var d = db.employees.ToList();
+            return Ok(d);
+        }
+
+        [Route("DelEmp/{id}")]
+        [HttpDelete]
+        public IActionResult DeleteEmp(int id)
+        {
+            var find = db.employees.Find(id);
+            db.employees.Remove(find);
+            db.SaveChanges();
+            return Ok("Emp Deleted Successfully!!");
+        }
+
+        [Route("UpdateEmp")]
+        [HttpPatch]
+
+        public IActionResult EditEmp(Emp emp)
+        {
+            db.employees.Update(emp);
+            db.SaveChanges();
+            return Ok("Emp Updated Successfully!!");
+        }
+    }
+}
---
+++ WebApiProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiProject.Models;
+
+namespace WebApiProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Emp> employees { get; set; }
+    }
+}
