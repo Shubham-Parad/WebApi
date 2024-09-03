a0987e06453dfbf4abcb36da9b007ff52800049b
Shubham-Parad <shubhamparad19@gmail.com>
2024-09-03 17:15:52 +0530

    multiple add, delete and search functionality added

--- WebApiProject/Controllers/EmpController.cs
+++ WebApiProject/Controllers/EmpController.cs
@@ -24,6 +24,15 @@ namespace WebApiProject.Controllers
             return Ok("Emp Added Successfully!!!");
         }
 
+        [Route("AddMultiple")]
+        [HttpPost]
+        public IActionResult AddEmpList(List<Emp> emp)
+        {
+            db.employees.AddRange(emp);
+            db.SaveChanges();
+            return Ok("Emp Added Successfully!!!");
+        }
+
         [Route("GetEmps")]
         [HttpGet]
         public IActionResult GetAllEmpData()
@@ -42,14 +51,33 @@ namespace WebApiProject.Controllers
             return Ok("Emp Deleted Successfully!!");
         }
 
+        [Route("DelMul")]
+        [HttpDelete]
+        public IActionResult DeleteMulEmp(List<int> id)
+        {
+
+            var data = db.employees.Where(x => id.Contains(x.Id)).ToList();
+            db.employees.RemoveRange(data);
+            db.SaveChanges();
+            return Ok("Emp Deleted Successfully!!");
+        }
+
         [Route("UpdateEmp")]
-        [HttpPatch]
+        [HttpPut]
 
         public IActionResult EditEmp(Emp emp)
         {
             db.employees.Update(emp);
             db.SaveChanges();
             return Ok("Emp Updated Successfully!!");
+        }
+
+        [Route("SearchEmp/{search}")]
+        [HttpGet]
+        public IActionResult SearchEmp(string search)
+        {
+            var result = db.employees.Where(x => search.Contains(x.Name) || search.Contains(x.Dept) || search.Contains(x.Salary.ToString()));
+            return Ok(result);
         }
     }
 }
--- WebApiProject/obj/Debug/net6.0/WebApiProject.AssemblyInfo.cs
+++ WebApiProject/obj/Debug/net6.0/WebApiProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ae5d6d3d55c84fe6e121797a35f1d07969e3780")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
